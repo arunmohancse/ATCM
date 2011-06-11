ca0e0b3580523bc56761c586656c88250c852a29
arun <arunmohancse@gmail.com>
2011-06-11 13:57:25 +0530

    changes in layout

--- atcm/application/controllers/ErrorController.php
+++ atcm/application/controllers/ErrorController.php
@@ -19,7 +19,7 @@ class ErrorController extends Zend_Controller_Action
                 // 404 error -- controller or action not found
                 $this->getResponse()->setHttpResponseCode(404);
                 $priority = Zend_Log::NOTICE;
-                $this->view->message = 'Page not found';
+                $this->view->message = '404 found';
                 break;
             default:
                 // application error
